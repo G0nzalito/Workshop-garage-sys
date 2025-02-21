fa8743069b5e90bb220e359cd64d6b75932cf0f9
G0nzalito <zavalagonzalo172@gmail.com>
2025-02-21 11:41:11 -0300

    Added the router and service of CC

---
+++ backend/src/routers/cobranzasRouter.ts
@@ -0,0 +1,70 @@
+import appExpress from "express"
+import {
+  getCobros,
+  getCobrosDesdeFecha,
+  registrarCobros,
+} from "../service/cobranzasService"
+import { getFormaDePagoById } from "../service/formaDePagoService"
+import { Database } from "../supabase/database.types"
+import { convertirFechaLocal } from "../support/supportFunctions"
+
+type Cobranza = Database["public"]["Tables"]["Cobranzas"]["Row"]
+type CobranzaAInsertar = Database["public"]["Tables"]["Cobranzas"]["Insert"]
+
+export const cajaRouter = appExpress.Router()
+
+cajaRouter.get("/all", async (req, res) => {
+  const cajas: Cobranza[] = await getCobros()
+
+  res.status(200).json(cajas)
+})
+
+cajaRouter.get("/specific", async (req, res) => {
+  const { fechaMinima, fechaMaxima } = req.body
+
+  const cobros = await getCobrosDesdeFecha(fechaMinima, fechaMaxima)
+
+  if (cobros) {
+    res.status(200).json(cobros)
+  } else {
+    res.status(404).json({ message: "Caja no encontrada" })
+  }
+})
+
+cajaRouter.post("/create", async (req, res) => {
+  try {
+    const cobro: CobranzaAInsertar = req.body
+
+    if (
+      cobro.Forma_de_Pago === undefined ||
+      cobro.Numero_Documento_Cliente === undefined ||
+      cobro.Tipo_Documento_Cliente === undefined ||
+      cobro.Monto === undefined ||
+      cobro.Turno === undefined
+    ) {
+      throw new ReferenceError("Faltan datos de la venta")
+    }
+
+    if (cobro.Forma_de_Pago === 2 || cobro.Forma_de_Pago === 3) {
+      if (
+        cobro.Autorizacion === undefined ||
+        cobro.Cupon === undefined ||
+        cobro.Lote === undefined
+      ) {
+        throw new ReferenceError("Faltan datos para la venta con tarjeta")
+      }
+    }
+
+    cobro.Fecha = convertirFechaLocal(new Date())
+
+    const caja = await registrarCobros(cobro)
+
+    res.status(201).json(caja)
+  } catch (e: unknown) {
+    if (e instanceof ReferenceError) {
+      res.status(400).json({ message: e.message })
+    } else {
+      res.status(500).json({ message: "Error interno del sistema", error: e })
+    }
+  }
+})
---
+++ backend/src/service/cobranzasService.ts
@@ -0,0 +1,72 @@
+import supabase from "../supabase/client"
+import { Database } from "../supabase/database.types"
+import {createAsientoContable} from './cajaContableService'
+
+type Cobranza = Database["public"]["Tables"]["Cobranzas"]["Row"]
+type CobranzaAInsertar = Database["public"]["Tables"]["Cobranzas"]["Insert"]
+
+async function getCobros() {
+  const { data, error } = await supabase.from("Cobranzas").select("*")
+
+  if (error) {
+    throw error
+  }
+  return data as Cobranza[]
+}
+
+async function getCobrosDesdeFecha(fechaMinima: string, fechaMaxima?: string) {
+  if (fechaMaxima === undefined) {
+    const { data, error } = await supabase
+      .from("Cobranzas")
+      .select("*")
+      .gte("Fecha", fechaMinima)
+
+    if (error) {
+      throw error
+    }
+    return data as Cobranza[]
+
+  }else{
+      const { data: data2, error: error2 } = await supabase
+        .from("Cobranzas")
+        .select("*")
+        .gte("Fecha", fechaMinima)
+        .lte("Fecha", fechaMaxima)
+  
+      if (error2) {
+        throw error2
+      }
+      return data2 as Cobranza[]
+  }
+}
+
+async function registrarCobros(cobranza: CobranzaAInsertar) {
+  const { data, error } = await supabase
+  .from("Cobranzas")
+  .insert(cobranza)
+  .select("*")
+  .limit(1)
+  .single()
+
+  if (error) {
+    throw error
+  }
+
+  if(cobranza.Forma_de_Pago === 1){
+    const asiento = {
+      Monto: cobranza.Monto,
+      Movimiento: 3,
+      Turno: cobranza.Turno,
+      Detalle: "Cobro de CC en efectivo",
+      Fecha: cobranza.Fecha
+    }
+    try {
+      await createAsientoContable(asiento)
+    } catch (error) {
+      throw error
+    }
+  }
+  return data as Cobranza
+}
+
+export { getCobros, getCobrosDesdeFecha, registrarCobros }
